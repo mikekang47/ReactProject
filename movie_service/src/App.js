2376dbc2497d56b63bc7ca98039926fd0dac5050
mikekang47 <gyeong0308@gmail.com>
2021-08-12 23:41:37 +0900

    Implement setstate method in project

--- movie_service/src/App.js
+++ movie_service/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-const foodILike= [{ } ]; 
- 
-function App() {
-  return <div>{foodILike.map(renderFood)}</div>
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {this.setState(current => ({count: current.count -1}))};
+  
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App; 
