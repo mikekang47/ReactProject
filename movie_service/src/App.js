a64238213f91fef3ff7c9e6f18f541a3b4debe3c
mikekang47 <gyeong0308@gmail.com>
2021-08-18 23:27:05 +0900

    Make Movie.js and get a movie's title

--- movie_service/src/App.js
+++ movie_service/src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios'; 
 import React from 'react';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -8,7 +9,7 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-      const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+      const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
       this.setState({movies, isLoading: false})
   };
 
@@ -17,13 +18,26 @@ class App extends React.Component {
 
   }
 
-  render(){
-    const {isLoading} = this.state;
-    return <div>{
-      isLoading ? "Loading..." : "We are ready"}
-      </div>;
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <div>
+      {
+        isLoading ? "Loading..." : 
+        movies.map(movie => (
+          <Movie 
+          key={movie.id} 
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary = {movie.summary} 
+          poster={movie.medium_cover_image} 
+          />
+      ))}
+      </div>
+    );
   }
 
 }
  
-export default App; 
+export default App;
